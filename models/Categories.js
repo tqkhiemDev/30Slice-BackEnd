2538e27dd0ed95f374083becbb9649a35f82abdf
zombieS12 <93500458+zombies1302@users.noreply.github.com>
2022-10-18 22:09:47 +0700

    Update Categories.js

--- models/Categories.js
+++ models/Categories.js
@@ -3,9 +3,9 @@ const mongoose = require("mongoose");
 const CategoriesSchema = new mongoose.Schema(
     {
         Name: { type: String, required: true },
-        Ordinal: { type: Number, required: true },
+        Ordinal: { type: Number, required: true, unique: true },
         Is_Show: { type: Boolean, required: true },
-        Parent_Id: { type: String , default: null},
+        Parent_Id: { type: String, default: null },
         isCatProducts: { type: Boolean, required: true },
     },
     { timestamps: true }
