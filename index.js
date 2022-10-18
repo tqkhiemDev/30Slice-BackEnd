8ba6d0a233ef945105570d5e3584ef26107486ef
zombieS12 <93500458+zombies1302@users.noreply.github.com>
2022-10-18 20:19:19 +0700

    commit

--- index.js
+++ index.js
@@ -4,13 +4,18 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const app = exp();
-
+const Mailjet = require('node-mailjet');
+dotenv.config();
 
 const cateRoute = require("./routes/categories");
 const proRoute = require("./routes/products");
 const orderRoute = require("./routes/order");
 
-dotenv.config();
+
+const mailjet = new Mailjet({
+  apiKey: process.env.MJ_APIKEY_PUBLIC || '',
+  apiSecret: process.env.MJ_APIKEY_PRIVATE || ''
+});
 mongoose.connect(process.env.MONGO_URL).then(() => console.log("DB Connection Successfull!")).catch((err) => {
   console.log(err);
 });
@@ -23,6 +28,53 @@ app.use("/api/products", proRoute);
 app.use("/api/orders", orderRoute);
 app.get("/",(req, res)=>{
   res.send("ahihi 1234")
+})
+
+app.post('/send-email', async (req, res) => {
+  try {
+    const request = await mailjet.post('send', { version: 'v3.1' }).request({
+      Messages: [
+        {
+          From: {
+            Email: 'no-reply@30slice.com',
+            Name: '30slice',
+          },
+          To: [
+            {
+              Email: 'tqkpro.dev@gmail.com',
+              Name: 'passenger 1',
+            },
+          ],
+          Variables: {
+            day: "Monday"
+          },
+          TemplateID: 4275347,
+          TemplateLanguage: true,
+          Subject: 'Lịch cắt tóc của bạn',
+        },
+
+      ],
+
+    })
+    console.log(request.body);
+    res.send(request.body);
+
+  }
+  catch (err) {
+    console.log(err);
+  }
+
+
+
+
+  //   request
+  //     .then(result => {
+  //       console.log(result.body)
+  //       res.send(result.body);
+  //     })
+  //     .catch(err => {
+  //       console.log(err.statusCode)
+  //     })
 })
 
 
