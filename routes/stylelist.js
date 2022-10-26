72559a197cab8ca1dfe4f6db0573acfb7ee8725c
zombieS12 <93500458+zombies1302@users.noreply.github.com>
2022-10-26 15:33:47 +0700

    Update stylelist.js

--- routes/stylelist.js
+++ routes/stylelist.js
@@ -1,5 +1,6 @@
 const Login = require("../models/Login");
 const StyleList = require("../models/Style_List");
+const Booking = require("../models/Booking");
 const router = require("express").Router();
 
 
@@ -69,8 +70,59 @@ router.post("/addShiftToStyleList", async (req, res) => {
   }
 })
 
+const getAvailableEmployee = (bookedDate, bookedTime) => {
+  if (!arrBookedData[bookedDate]) {
+    arrBookedData[bookedDate] = new Array(allAvailableTime.length).fill([])
+  }
+  const arrBookedEmployee = arrBookedData[bookedDate][bookedTime]
+  return arrEmployee.filter(employee => {
+    return arrBookedEmployee.findIndex(booking => {
+      return booking.employee.employeeID === employee.employeeID
+    }) === -1
+  })
+}
+router.get('/getAvailableEmployee', async (req, res) => {
+  const bookedDate = req.query.bookedDate
+  try {
+    const arrBooked = await Booking.find({ BookedDate: bookedDate });
+    const arrStyle_lists = await Login.aggregate([
+      {
+        $lookup: {
+          from: "style_lists",
+          localField: "_id",
+          foreignField: "Id_User",
+          as: "Info"
+        },
+      },
+      {
+        $match: {
+          "Role": "styleList",
+        }
+      },
+      {
+        $unwind: "$Info"
+      },
+      {
+        $project: {
+          "Password": 0,
+          "__v": 0,
+          "Info.__v": 0,
+          "Info.Id_User": 0,
+          "Info.createdAt": 0,
+          "Info.updatedAt": 0,
+        }
+      }
+    ])
+   
 
-    
+    res.status(200).json({ arrBooked, arrStyle_lists });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+})
+
+
+
 
 
 
