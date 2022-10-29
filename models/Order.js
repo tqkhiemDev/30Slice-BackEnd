926c8d4cdcefd07571b316e2c4a2a95f11fc599f
zombieS12 <93500458+zombies1302@users.noreply.github.com>
2022-10-29 21:49:25 +0700

    Update Order.js

--- models/Order.js
+++ models/Order.js
@@ -3,19 +3,38 @@ const mongoose = require("mongoose");
 const OrderSchema = new mongoose.Schema(
     {
         Id_Customer: { type: mongoose.Schema.Types.ObjectId, ref: 'customer' },
-        Id_OrderDetail: { type: mongoose.Schema.Types.ObjectId, ref: 'Order_Detail' },
-        Status: { type: String,require: true},
-        Receiver: { type: String},
+        Products: [
+            {
+                ProductId: {
+                    type: mongoose.Schema.Types.ObjectId, ref: 'product'
+                },
+                Quantity: {
+                    type: Number,
+                    default: 1,
+                },
+                Name: {
+                    type: String
+                },
+                Price: {
+                    type: Number
+                },
+                Images: {
+                    type: Array
+                }
+            },
+        ],
+        Status: { type: String, require: true },
+        Receiver: { type: String },
         Address: { type: String, required: true },
         Phone: { type: String, required: true },
         Email: { type: String, required: true },
-        Amount : {type : String , required: true},
-        Payment_Method : {type : String},
-        Payment_Status : {type : String},
-        Admin_Note : {type : String},
-        Customer_Note : {type : String},
-        IsCustomer_Delete : {type : Boolean , default : false},
-        IsAdmin_Delete : {type : Boolean , default : false},
+        Amount: { type: String, required: true },
+        Payment_Method: { type: String },
+        Payment_Status: { type: String },
+        Admin_Note: { type: String },
+        Customer_Note: { type: String },
+        IsCustomer_Delete: { type: Boolean, default: false },
+        IsAdmin_Delete: { type: Boolean, default: false },
     },
     { timestamps: true }
 );
