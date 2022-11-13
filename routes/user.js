71eba852f519d3d7aff9418c6c127724cf8049d9
zombieS12 <93500458+zombies1302@users.noreply.github.com>
2022-11-13 16:11:08 +0700

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,13 @@
-const Login = require('../models/Login');
-const Customer = require('../models/Customer');
-const router = require('express').Router();
+const Login = require("../models/Login");
+const Customer = require("../models/Customer");
+const router = require("express").Router();
+const jwt = require('jsonwebtoken');
+const fs = require('fs');
+const PRIVATE_KEY = fs.readFileSync('./private-key.txt');
 function makeid(length) {
-  var result = '';
+  var result = "";
   var characters =
-    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   var charactersLength = characters.length;
   for (var i = 0; i < length; i++) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength));
@@ -12,7 +15,7 @@ function makeid(length) {
   return result;
 }
 // customer register
-router.post('/register', async (req, res) => {
+router.post("/register", async (req, res) => {
   try {
     const newUser = new Login({
       Username: makeid(15),
@@ -37,24 +40,68 @@ router.post('/register', async (req, res) => {
   }
 });
 // booking by customer
-router.post('/booking', async (req, res) => {
+router.post("/booking", async (req, res) => {
   try {
     const user = await Login.findOne({ Phone: req.body.phone });
     if (user) {
-      res
-        .status(200)
-        .json({
-          Id_User: user._id,
-          Full_Name: user.Full_Name,
-          Phone: user.Phone,
-        });
+      res.status(200).json({
+        Id_User: user._id,
+        Full_Name: user.Full_Name,
+        Phone: user.Phone,
+      });
     } else {
       res
         .status(401)
-        .json({ message: 'Số điện thoại này chưa đăng ký', status_code: 404 });
+        .json({ message: "Số điện thoại này chưa đăng ký", status_code: 404 });
     }
   } catch (err) {
     res.status(500).json(err);
+  }
+});
+// user sign up
+router.post("/signup", async (req, res) => {
+  try {
+    const newUser = new Login({
+      Username: req.body.username,
+      Password: req.body.password,
+      Email: req.body.email,
+      Full_Name: req.body.full_name,
+      Phone: req.body.phone,
+    });
+    const user = await newUser.save();
+    const newCustomer = new Customer({
+      Id_User: user._id,
+    });
+    const customer = await newCustomer.save();
+    const { Password , ...other} = user._doc
+
+ 
+
+    res.status(200).json(other);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+// user sign in
+router.post("/signin", async (req, res) => {
+  try {
+    const user = await Login.findOne({ Username: req.body.username });
+    if (user) {
+      // statement
+      if ( user.Password !== req.body.password ){
+        res.status(401).json({"message": "Thông tin đăng nhập không đúng.","status_code": 401});
+      }else{
+        const accessToken = jwt.sign(
+          { id: user._id,role: user.Role },PRIVATE_KEY,{ expiresIn: "12h" });
+        res.status(200).json({ accessToken,role: user.Role,username: user.Username });
+      }
+    }else{
+      res.status(401).json({"message": "Thông tin đăng nhập không đúng.","status_code": 401});
+    }
+  }
+  catch(e){
+    console.log(e)
+    res.status(500).json(e);
   }
 });
 
