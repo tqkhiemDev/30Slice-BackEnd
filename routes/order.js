b56f4a2ec22091031045c263e55ce1ba24b57c18
zombieS12 <93500458+zombies1302@users.noreply.github.com>
2022-11-13 18:29:50 +0700

    update thanh toán vnpay

--- routes/order.js
+++ routes/order.js
@@ -1,7 +1,21 @@
 const Order = require("../models/Order");
-
+var dateFormat = require('dateformat');
 const router = require("express").Router();
-
+function sortObject(obj) {
+	var sorted = {};
+	var str = [];
+	var key;
+	for (key in obj){
+		if (obj.hasOwnProperty(key)) {
+		str.push(encodeURIComponent(key));
+		}
+	}
+	str.sort();
+    for (key = 0; key < str.length; key++) {
+        sorted[str[key]] = encodeURIComponent(obj[str[key]]).replace(/%20/g, "+");
+    }
+    return sorted;
+}
 //get all
 router.get("/getAllOrders", async (req, res) => {
   try {
@@ -73,4 +87,53 @@ router.put("CancelOrderByUser", async (req, res) => {
       res.status(400).json(err);
     }
 });
+
+router.post('/create_payment_url', function (req, res, next) {
+  var ipAddr = req.headers['x-forwarded-for'] ||
+      req.connection.remoteAddress ||
+      req.socket.remoteAddress ||
+      req.connection.socket.remoteAddress;
+
+  var tmnCode = process.env.vnp_TmnCode;
+  var secretKey = process.env.vnp_HashSecret;
+  var vnpUrl = process.env.vnp_Url;
+  var returnUrl = process.env.vnp_ReturnUrl;
+  var date = new Date()
+
+
+  var createDate = dateFormat(date, 'yyyymmddHHmmss');;
+  var orderId = req.body.orderId;;
+  var amount = req.body.amount;
+  var bankCode = '';
+  
+  var vnp_Params = {};
+  vnp_Params['vnp_Version'] = '2.1.0';
+  vnp_Params['vnp_Command'] = 'pay';
+  vnp_Params['vnp_TmnCode'] = tmnCode;
+  vnp_Params['vnp_Locale'] = 'vn';
+  vnp_Params['vnp_CurrCode'] = 'VND';
+  vnp_Params['vnp_TxnRef'] = orderId;
+  vnp_Params['vnp_OrderInfo'] = "Thanh toán đơn hàng 30Slice";
+  vnp_Params['vnp_OrderType'] = "billpayment";
+  vnp_Params['vnp_Amount'] = amount * 100;
+  vnp_Params['vnp_ReturnUrl'] = returnUrl;
+  vnp_Params['vnp_IpAddr'] = ipAddr;
+  vnp_Params['vnp_CreateDate'] = createDate;
+  if(bankCode !== null && bankCode !== ''){
+      vnp_Params['vnp_BankCode'] = bankCode;
+  }
+
+  vnp_Params = sortObject(vnp_Params);
+
+  var querystring = require('qs');
+  var signData = querystring.stringify(vnp_Params, { encode: false });
+  var crypto = require("crypto");     
+  var hmac = crypto.createHmac("sha512", secretKey);
+  var signed = hmac.update(new Buffer(signData, 'utf-8')).digest("hex"); 
+  vnp_Params['vnp_SecureHash'] = signed;
+  vnpUrl += '?' + querystring.stringify(vnp_Params, { encode: false });
+  console.log(vnpUrl)
+  res.json(vnpUrl)
+});
+
 module.exports = router;
