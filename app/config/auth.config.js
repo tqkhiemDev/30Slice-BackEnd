225ff0e36cebefb82962474c2196dab99b0ba089
cuongha <cuongha0701@gmail.com>
2022-11-18 16:31:08 +0700

    update config

--- app/config/auth.config.js
+++ app/config/auth.config.js
@@ -3,9 +3,9 @@ const PRIVATE_KEY = fs.readFileSync('./private-key.txt');
 
 module.exports = {
   secret: PRIVATE_KEY,
-  // jwtExpiration: 3600,         // 1 hour
-  // jwtRefreshExpiration: 86400, // 24 hours
+  jwtExpiration: 3600, // 1 hour
+  jwtRefreshExpiration: 86400, // 24 hours
 
-  jwtExpiration: 5, // 30 second
-  jwtRefreshExpiration: 60 * 60, // 1 hour
+  // jwtExpiration: 60, // 60 second
+  // jwtRefreshExpiration: 60 * 60, // 1 hour
 };
