6e7cbd3f2372c37cf7b2070c8f451bb7b8bf3cb6
cuongha <cuongha0701@gmail.com>
2022-11-18 12:52:16 +0700

    update veryfyToken

--- app/middlewares/auth/authJwt.js
+++ app/middlewares/auth/authJwt.js
@@ -16,7 +16,7 @@ const catchError = (err, res) => {
 };
 
 const verifyToken = (req, res, next) => {
-  let token = req.headers?.Authorization.replace('Bearer ', '');
+  let token = req.headers?.authorization.replace('Bearer ', '');
   if (!token) {
     return res.status(403).send({ message: 'No token provided!' });
   }
