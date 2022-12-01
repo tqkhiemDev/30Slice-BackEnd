f4f94ec20c53bb390bc9e7c5ca99cac526f0186d
zombieS12 <93500458+zombies1302@users.noreply.github.com>
2022-12-01 14:13:56 +0700

    a

--- app/models/Order.js
+++ app/models/Order.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const OrderSchema = new mongoose.Schema(
   {
-    Id_Customer: { type: mongoose.Schema.Types.ObjectId, ref: "customer" },
+    Id_Customer: { type: mongoose.Schema.Types.ObjectId, ref: "logins" },
     Products: [
       {
         _id: {
--- app/routes/order.js
+++ app/routes/order.js
@@ -1,30 +1,63 @@
 const router = require("express").Router();
 const Order = require("../models/Order");
-
-const querystring = require('qs');
-const crypto = require("crypto"); 
-const dateFormat = require('dateformat');
+const querystring = require("qs");
+const crypto = require("crypto");
+const dateFormat = require("dateformat");
 
 function sortObject(obj) {
-	var sorted = {};
-	var str = [];
-	var key;
-	for (key in obj){
-		if (obj.hasOwnProperty(key)) {
-		str.push(encodeURIComponent(key));
-		}
-	}
-	str.sort();
-    for (key = 0; key < str.length; key++) {
-        sorted[str[key]] = encodeURIComponent(obj[str[key]]).replace(/%20/g, "+");
+  var sorted = {};
+  var str = [];
+  var key;
+  for (key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      str.push(encodeURIComponent(key));
     }
-    return sorted;
+  }
+  str.sort();
+  for (key = 0; key < str.length; key++) {
+    sorted[str[key]] = encodeURIComponent(obj[str[key]]).replace(/%20/g, "+");
+  }
+  return sorted;
 }
 //get all
 router.get("/getAllOrders", async (req, res) => {
   try {
     const orders = await Order.find();
     res.status(200).json(orders);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+// get one order using aggregate
+router.get("/getOneOrder/:id", async (req, res) => {
+  try {
+    const order = await Order.aggregate([
+      {
+        $lookup: {
+          from: "logins",
+          localField: "Id_Customer",
+          foreignField: "_id",
+          as: "Info",
+        },
+      },
+      {
+        $match: {
+          _id: req.params.id,
+        },
+      },
+      {
+        $unwind: "$Info",
+      },
+      {
+        $project: {
+          "Info.Password": 0,
+          "Info.__v": 0,
+          "Info.createdAt": 0,
+          "Info.updatedAt": 0,
+        },
+      },
+    ]);
+    res.status(200).json(order);
   } catch (err) {
     res.status(400).json(err);
   }
@@ -68,73 +101,67 @@ router.put("DeleteOrderByUser", async (req, res) => {
 
 //admin hidden
 router.put("DeleteOrderByAdmin", async (req, res) => {
-    try {
-      await Order.findOneAndUpdate(
-        { _id: req.body._id },
-        { IsAdmin_Delete: true }
-      );
-      res.status(200).json("Update thành công");
-    } catch (err) {
-      res.status(400).json(err);
-    }
+  try {
+    await Order.findOneAndUpdate(
+      { _id: req.body._id },
+      { IsAdmin_Delete: true }
+    );
+    res.status(200).json("Update thành công");
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 //cancel order
 router.put("CancelOrderByUser", async (req, res) => {
-    try {
-      await Order.findOneAndUpdate(
-        { _id: req.body._id },
-        { Status: "huy" }
-      );
-      res.status(200).json("Update thành công");
-    } catch (err) {
-      res.status(400).json(err);
-    }
+  try {
+    await Order.findOneAndUpdate({ _id: req.body._id }, { Status: "huy" });
+    res.status(200).json("Update thành công");
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.post('/create_payment_url', function (req, res, next) {
-  var ipAddr = req.headers['x-forwarded-for'] ||
-      req.connection.remoteAddress ||
-      req.socket.remoteAddress ||
-      req.connection.socket.remoteAddress;
-      console.log(ipAddr)
+router.post("/create_payment_url", function (req, res, next) {
+  var ipAddr =
+    req.headers["x-forwarded-for"] ||
+    req.connection.remoteAddress ||
+    req.socket.remoteAddress ||
+    req.connection.socket.remoteAddress;
+  console.log(ipAddr);
 
   var tmnCode = process.env.vnp_TmnCode;
   var secretKey = process.env.vnp_HashSecret;
   var vnpUrl = process.env.vnp_Url;
   var returnUrl = process.env.vnp_ReturnUrl;
-  var date = new Date()
+  var date = new Date();
 
-
-  var createDate = dateFormat(date, 'yyyymmddHHmmss');;
+  var createDate = dateFormat(date, "yyyymmddHHmmss");
   var orderId = req.body.orderId;
-  
-  var vnp_Params = {};
-  vnp_Params['vnp_Version'] = '2.1.0';
-  vnp_Params['vnp_Command'] = 'pay';
-  vnp_Params['vnp_TmnCode'] = tmnCode;
-  vnp_Params['vnp_Locale'] = 'vn';
-  vnp_Params['vnp_CurrCode'] = 'VND';
-  vnp_Params['vnp_TxnRef'] = orderId;
-  vnp_Params['vnp_OrderInfo'] = "Thanh toán đơn hàng 30Slice " + orderId;
-  vnp_Params['vnp_OrderType'] = "billpayment";
-  vnp_Params['vnp_Amount'] = req.body.amount * 100;
-  vnp_Params['vnp_ReturnUrl'] = returnUrl;
-  vnp_Params['vnp_IpAddr'] = ipAddr;
-  vnp_Params['vnp_CreateDate'] = createDate;
 
+  var vnp_Params = {};
+  vnp_Params["vnp_Version"] = "2.1.0";
+  vnp_Params["vnp_Command"] = "pay";
+  vnp_Params["vnp_TmnCode"] = tmnCode;
+  vnp_Params["vnp_Locale"] = "vn";
+  vnp_Params["vnp_CurrCode"] = "VND";
+  vnp_Params["vnp_TxnRef"] = orderId;
+  vnp_Params["vnp_OrderInfo"] = "Thanh toán đơn hàng 30Slice " + orderId;
+  vnp_Params["vnp_OrderType"] = "billpayment";
+  vnp_Params["vnp_Amount"] = req.body.amount * 100;
+  vnp_Params["vnp_ReturnUrl"] = returnUrl;
+  vnp_Params["vnp_IpAddr"] = ipAddr;
+  vnp_Params["vnp_CreateDate"] = createDate;
 
   vnp_Params = sortObject(vnp_Params);
 
-     
-
   var signData = querystring.stringify(vnp_Params, { encode: false });
   var hmac = crypto.createHmac("sha512", secretKey);
-  var signed = hmac.update(new Buffer(signData, 'utf-8')).digest("hex"); 
-  vnp_Params['vnp_SecureHash'] = signed;
-  vnpUrl += '?' + querystring.stringify(vnp_Params, { encode: false });
+  var signed = hmac.update(new Buffer(signData, "utf-8")).digest("hex");
+  vnp_Params["vnp_SecureHash"] = signed;
+  vnpUrl += "?" + querystring.stringify(vnp_Params, { encode: false });
 
-  res.status(200).json(vnpUrl)
+  res.status(200).json(vnpUrl);
 });
 
 module.exports = router;
