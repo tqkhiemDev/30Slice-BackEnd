0c15ea15da5385a485e1fd9ce17048768122f221
zombieS12 <93500458+zombies1302@users.noreply.github.com>
2022-12-02 16:07:45 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const exp = require('express');
+const exp = require("express");
 const app = exp();
-const cors = require('cors');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const Mailjet = require('node-mailjet');
-const { logger, logEvents } = require('./app/middlewares/auth/logger');
-const errorHandler = require('./app/middlewares/errorHandler');
+const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const Mailjet = require("node-mailjet");
+const { logger, logEvents } = require("./app/middlewares/auth/logger");
+const errorHandler = require("./app/middlewares/errorHandler");
 
 dotenv.config();
 app.use(cors());
@@ -13,97 +13,52 @@ app.use(exp.json());
 app.use(logger);
 app.use(errorHandler);
 
-const orderRoute = require('./app/routes/order');
-const userRoute = require('./app/routes/user');
-const adminRoute = require('./app/routes/admin');
-const categoryRoute = require('./app/routes/category');
-const serviceRoute = require('./app/routes/service');
-const styleListRoute = require('./app/routes/stylelist');
-const productRoute = require('./app/routes/product');
-const newsRoute = require('./app/routes/news');
-const bookingRoute = require('./app/routes/booking');
-const comboRoute = require('./app/routes/combos');
-const userMngt = require('./app/routes/admin/userMngt.route');
+const orderRoute = require("./app/routes/order");
+const userRoute = require("./app/routes/user");
+const adminRoute = require("./app/routes/admin");
+const categoryRoute = require("./app/routes/category");
+const serviceRoute = require("./app/routes/service");
+const styleListRoute = require("./app/routes/stylelist");
+const productRoute = require("./app/routes/product");
+const newsRoute = require("./app/routes/news");
+const bookingRoute = require("./app/routes/booking");
+const comboRoute = require("./app/routes/combos");
+const userMngt = require("./app/routes/admin/userMngt.route");
 
-app.use('/api/order', orderRoute);
-app.use('/api/user', userRoute);
-app.use('/api/admin', adminRoute);
-app.use('/api/category', categoryRoute);
-app.use('/api/service', serviceRoute);
-app.use('/api/stylelist', styleListRoute);
-app.use('/api/product', productRoute);
-app.use('/api/news', newsRoute);
-app.use('/api/booking', bookingRoute);
-app.use('/api/combo', comboRoute);
-app.use('/api/admin/user-management', userMngt);
-
+app.use("/api/order", orderRoute);
+app.use("/api/user", userRoute);
+app.use("/api/admin", adminRoute);
+app.use("/api/category", categoryRoute);
+app.use("/api/service", serviceRoute);
+app.use("/api/stylelist", styleListRoute);
+app.use("/api/product", productRoute);
+app.use("/api/news", newsRoute);
+app.use("/api/booking", bookingRoute);
+app.use("/api/combo", comboRoute);
+app.use("/api/admin/user-management", userMngt);
 
 // Refresh token
-const authRoute = require('./app/routes/auth/auth.routes');
-const userTestRoute = require('./app/routes/auth/user.routes');
-app.use('/api/auth', authRoute);
-app.use('/api/user/test', userTestRoute);
+const authRoute = require("./app/routes/auth/auth.routes");
+const userTestRoute = require("./app/routes/auth/user.routes");
+app.use("/api/auth", authRoute);
+app.use("/api/user/test", userTestRoute);
 
 mongoose
   .connect(process.env.MONGO_URL)
-  .then(() => console.log('DB Connection Successfull!'))
+  .then(() => {
+    console.log("DB Connection Successfull!");
+    app.listen(process.env.PORT, () => {
+      console.log(`Ung dung dang chay voi port ${process.env.PORT}`);
+    });
+  })
   .catch((err) => {
     console.log(err);
   });
 
-mongoose.connection.on('error', (err) => {
+mongoose.connection.on("error", (err) => {
   console.log(err);
   logEvents(
     `${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,
-    'mongoErrLog.log'
+    "mongoErrLog.log"
   );
-});
-
-const mailjet = new Mailjet({
-  apiKey: process.env.MJ_APIKEY_PUBLIC || '',
-  apiSecret: process.env.MJ_APIKEY_PRIVATE || '',
-});
-
-app.post('/send-email', async (req, res) => {
-  try {
-    const request = await mailjet.post('send', { version: 'v3.1' }).request({
-      Messages: [
-        {
-          From: {
-            Email: 'no-reply@30slice.com',
-            Name: '30slice',
-          },
-          To: [
-            {
-              Email: 'tqkpro.dev@gmail.com',
-              Name: 'passenger 1',
-            },
-          ],
-          Variables: {
-            day: 'Monday',
-          },
-          TemplateID: 4275347,
-          TemplateLanguage: true,
-          Subject: 'Lịch cắt tóc của bạn',
-        },
-      ],
-    });
-    console.log(request.body);
-    res.send(request.body);
-  } catch (err) {
-    console.log(err);
-  }
-
-  //   request
-  //     .then(result => {
-  //       console.log(result.body)
-  //       res.send(result.body);
-  //     })
-  //     .catch(err => {
-  //       console.log(err.statusCode)
-  //     })
-});
-
-app.listen(process.env.PORT, () => {
-  console.log(`Ung dung dang chay voi port ${process.env.PORT}`);
 });
