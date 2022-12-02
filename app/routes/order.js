64653b488e7a0929dc3b9d4837e996208ccb9770
zombieS12 <93500458+zombies1302@users.noreply.github.com>
2022-12-02 13:57:51 +0700

    Update order.js

--- app/routes/order.js
+++ app/routes/order.js
@@ -176,24 +176,28 @@ router.post("/orderVnpay", async (req, res, next) => {
   }
 });
 router.get("/vnpay_return", async (req, res, next) => {
-  var vnp_Params = req.query;
-  var secureHash = vnp_Params["vnp_SecureHash"];
-  delete vnp_Params["vnp_SecureHash"];
-  delete vnp_Params["vnp_SecureHashType"];
-  vnp_Params = sortObject(vnp_Params);
-  var tmnCode = process.env.vnp_TmnCode;
-  var secretKey = process.env.vnp_HashSecret;
-  var signData = querystring.stringify(vnp_Params, { encode: false });
+  try {
+    var vnp_Params = req.query;
+    var secureHash = vnp_Params["vnp_SecureHash"];
+    delete vnp_Params["vnp_SecureHash"];
+    delete vnp_Params["vnp_SecureHashType"];
+    vnp_Params = sortObject(vnp_Params);
+    var tmnCode = process.env.vnp_TmnCode;
+    var secretKey = process.env.vnp_HashSecret;
+    var signData = querystring.stringify(vnp_Params, { encode: false });
 
-  var hmac = crypto.createHmac("sha512", secretKey);
-  var signed = hmac.update(new Buffer(signData, "utf-8")).digest("hex");
+    var hmac = crypto.createHmac("sha512", secretKey);
+    var signed = hmac.update(new Buffer(signData, "utf-8")).digest("hex");
 
-  if (secureHash === signed) {
-    //Kiem tra xem du lieu trong db co hop le hay khong va thong bao ket qua
+    if (secureHash === signed) {
+      //Kiem tra xem du lieu trong db co hop le hay khong va thong bao ket qua
 
-    res.render("success", { code: vnp_Params["vnp_ResponseCode"] });
-  } else {
-    res.render("success", { code: "97" });
+      res.render("success", { code: vnp_Params["vnp_ResponseCode"] });
+    } else {
+      res.render("success", { code: "97" });
+    }
+  } catch (err) {
+    res.status(400).json(err);
   }
 });
 
