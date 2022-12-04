9f01bf641090062c30b350db79393de912ab0422
zombieS12 <93500458+zombies1302@users.noreply.github.com>
2022-12-04 14:05:47 +0700

    cm

--- app/routes/product.js
+++ app/routes/product.js
@@ -48,7 +48,7 @@ router.put(
 //show
 router.get('/getAllProducts', async (req, res) => {
   try {
-    const products = await Product.find().sort({ createdAt: -1 });
+    const products = await Product.find().sort({ createdAt: -1 }).populate("Id_Categories","Name");
     res.status(200).json(products);
   } catch (err) {
     res.status(400).json(err);
