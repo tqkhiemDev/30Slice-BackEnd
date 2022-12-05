53bf070719671ce2b5f4e49cac2c83920c57d6af
zombieS12 <93500458+zombies1302@users.noreply.github.com>
2022-12-05 10:44:01 +0700

    update api get product admin phân trang and search

--- app/routes/product.js
+++ app/routes/product.js
@@ -1,10 +1,10 @@
-const Product = require('../models/Product');
-const { authJwt } = require('../middlewares/auth');
+const Product = require("../models/Product");
+const { authJwt } = require("../middlewares/auth");
 
-const router = require('express').Router();
+const router = require("express").Router();
 
 //them
-router.post('/', [authJwt.verifyToken, authJwt.isAdmin], async (req, res) => {
+router.post("/", [authJwt.verifyToken, authJwt.isAdmin], async (req, res) => {
   const newProduct = new Product(req.body);
   try {
     const savedProduct = await newProduct.save();
@@ -15,7 +15,7 @@ router.post('/', [authJwt.verifyToken, authJwt.isAdmin], async (req, res) => {
 });
 
 //sua
-router.put('/', [authJwt.verifyToken, authJwt.isAdmin], async (req, res) => {
+router.put("/", [authJwt.verifyToken, authJwt.isAdmin], async (req, res) => {
   try {
     const updatedProduct = await Product.findByIdAndUpdate(
       req.body._id,
@@ -30,7 +30,7 @@ router.put('/', [authJwt.verifyToken, authJwt.isAdmin], async (req, res) => {
 
 //toggle isShow
 router.put(
-  '/changeHideOrShow',
+  "/changeHideOrShow",
   [authJwt.verifyToken, authJwt.isAdmin],
   async (req, res) => {
     try {
@@ -38,7 +38,7 @@ router.put(
         { _id: req.body._id },
         { Is_Show: req.body.Is_Show }
       );
-      res.status(200).json('change success!!');
+      res.status(200).json("change success!!");
     } catch (err) {
       res.status(400).json(err);
     }
@@ -46,23 +46,25 @@ router.put(
 );
 
 //show
-router.get('/getAllProducts', async (req, res) => {
-  try {
-    const products = await Product.find().sort({ createdAt: -1 }).populate("Id_Categories","Name");
-    res.status(200).json(products);
-  } catch (err) {
-    res.status(400).json(err);
-  }
-});
+// router.get('/getAllProducts', async (req, res) => {
+//   try {
+//     const products = await Product.find().sort({ createdAt: -1 }).populate("Id_Categories","Name");
+//     res.status(200).json(products);
+//   } catch (err) {
+//     res.status(400).json(err);
+//   }
+// });
 
 //show by category
-router.get('/getProductsByCategory/:id', async (req, res) => {
+router.get("/getProductsByCategory/:id", async (req, res) => {
   const Id_Categories = req.params.id;
   try {
     const products = await Product.find({
       Id_Categories: Id_Categories,
       Is_Show: true,
-    }).sort({ createdAt: -1 }).populate('Id_Categories', 'Name');
+    })
+      .sort({ createdAt: -1 })
+      .populate("Id_Categories", "Name");
 
     res.status(200).json(products);
   } catch (err) {
@@ -71,12 +73,12 @@ router.get('/getProductsByCategory/:id', async (req, res) => {
 });
 
 // get one product and get views +1
-router.get('/getOneProduct/:id', async (req, res) => {
+router.get("/getOneProduct/:id", async (req, res) => {
   const Id_Product = req.params.id;
   try {
     const products = await Product.findById(Id_Product).populate(
-      'Id_Categories',
-      'Name'
+      "Id_Categories",
+      "Name"
     );
     const updatedProduct = await Product.findOneAndUpdate(
       { _id: Id_Product },
@@ -89,21 +91,21 @@ router.get('/getOneProduct/:id', async (req, res) => {
 });
 
 // get product by page and limit and return total page
-router.get('/getProducts', async (req, res) => {
+router.get("/getProducts", async (req, res) => {
   const page = req.query.page;
   const limit = req.query.limit;
   const name = decodeURI(req.query.search);
   try {
     if (name) {
       const products = await Product.find({
-        Name: { $regex: name, $options: 'i' },
+        Name: { $regex: name, $options: "i" },
         Is_Show: true,
       })
         .sort({ createdAt: -1 })
         .skip((page - 1) * limit)
         .limit(limit * 1);
       const totalItem = await Product.countDocuments({
-        Name: { $regex: name, $options: 'i' },
+        Name: { $regex: name, $options: "i" },
       });
       const totalPage = Math.ceil(totalItem / limit);
       res.status(200).json({ totalItem, totalPage, products });
@@ -120,16 +122,46 @@ router.get('/getProducts', async (req, res) => {
     res.status(400).json(err);
   }
 });
-
-router.delete('/',async (req,res) => {
+// get product by page and limit and return total page by admin
+router.get("/getAllProducts",[authJwt.verifyToken,authJwt.isAdmin], async (req, res) => {
+  const page = req.query.page;
+  const limit = req.query.limit;
+  const name = decodeURI(req.query.search);
   try {
-    await Product.findByIdAndDelete(
-      { _id: req.body._id }
-    );
-    res.status(200).json('delete success!!');
+    if (name) {
+      const products = await Product.find({
+        Name: { $regex: name, $options: "i" },
+      })
+        .populate("Id_Categories", "Name")
+        .sort({ createdAt: -1 })
+        .skip((page - 1) * limit)
+        .limit(limit * 1);
+      const totalItem = await Product.countDocuments({
+        Name: { $regex: name, $options: "i" },
+      });
+      const totalPage = Math.ceil(totalItem / limit);
+      res.status(200).json({ totalItem, totalPage, products });
+    } else {
+      const totalItem = await Product.countDocuments();
+      const totalPage = Math.ceil(totalItem / limit);
+      const products = await Product.find()
+        .populate("Id_Categories", "Name")
+        .sort({ createdAt: -1 })
+        .skip((page - 1) * limit)
+        .limit(limit * 1);
+      res.status(200).json({ totalItem, totalPage, products });
+    }
   } catch (err) {
     res.status(400).json(err);
   }
-})
+});
+router.delete("/", async (req, res) => {
+  try {
+    await Product.findByIdAndDelete({ _id: req.body._id });
+    res.status(200).json("delete success!!");
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
 module.exports = router;
