1b315c52ed18c2f77ab624dfcd7906126d0cf9cb
zombieS12 <93500458+zombies1302@users.noreply.github.com>
2022-12-05 16:33:16 +0700

    Update Customer.js

--- app/models/Customer.js
+++ app/models/Customer.js
@@ -3,9 +3,6 @@ const mongoose = require("mongoose");
 const CustomerSchema = new mongoose.Schema(
   {
     Id_User: { type: mongoose.Schema.Types.ObjectId, ref: "login" },
-    History: { type: Array },
-    Style_List_Favorite: { type: String, default: null },
-    Schedule: { type: String, default: null },
     isSignUp: { type: Boolean, default: false },
     Block_By_Admin: { type: Number, required: true, default: 1 },
   },
