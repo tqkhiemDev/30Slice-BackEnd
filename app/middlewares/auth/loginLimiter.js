ae1482c675753dcfce89037804569520bfa63150
zombieS12 <93500458+zombies1302@users.noreply.github.com>
2022-12-07 21:00:46 +0700

    cmt log

--- app/middlewares/auth/loginLimiter.js
+++ app/middlewares/auth/loginLimiter.js
@@ -9,10 +9,10 @@ const loginLimiter = rateLimit({
       'Too many login attempts from this IP, please try again after a 60 second pause',
   },
   handler: (req, res, next, options) => {
-    logEvents(
-      `Too Many Requests: ${options.message.message}\t${req.method}\t${req.url}\t${req.headers.origin}`,
-      'errLog.log'
-    );
+    // logEvents(
+    //   `Too Many Requests: ${options.message.message}\t${req.method}\t${req.url}\t${req.headers.origin}`,
+    //   'errLog.log'
+    // );
     res.status(options.statusCode).send(options.message);
   },
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
