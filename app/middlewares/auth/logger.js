70dc94e737e8a21ca8215c58da2a0f66ae8b211e
Cuong Ha <cuongha0701@gmail.com>
2022-12-09 16:25:55 +0700

    update

--- app/middlewares/auth/logger.js
+++ app/middlewares/auth/logger.js
@@ -9,11 +9,11 @@ const logEvents = async (message, logFileName) => {
   const logItem = `${dateTime}\t${uuid()}\t${message}\n`;
 
   try {
-    if (!fs.existsSync(path.join(__dirname, '../../..', 'logs'))) {
-      await fsPromises.mkdir(path.join(__dirname, '../../..', 'logs'));
+    if (!fs.existsSync(path.join(__dirname, '../..', 'logs'))) {
+      await fsPromises.mkdir(path.join(__dirname, '../..', 'logs'));
     }
     await fsPromises.appendFile(
-      path.join(__dirname, '../../..', 'logs', logFileName),
+      path.join(__dirname, '../..', 'logs', logFileName),
       logItem
     );
   } catch (err) {
