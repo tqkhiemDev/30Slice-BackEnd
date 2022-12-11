3bae190b2ad5e49e78ea6fb1d0a027116ba45882
zombieS12 <93500458+zombies1302@users.noreply.github.com>
2022-12-11 19:56:15 +0700

    Update combos.js

--- app/routes/combos.js
+++ app/routes/combos.js
@@ -1,9 +1,9 @@
-const Combos = require('../models/Combos');
-
-const router = require('express').Router();
+const Combos = require("../models/Combos");
+const mongoose = require("mongoose");
+const router = require("express").Router();
 
 // add bookings to the database
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   const newCombos = new Combos(req.body);
   try {
     const saveCombos = await newCombos.save();
@@ -14,7 +14,7 @@ router.post('/', async (req, res) => {
 });
 
 // get combos for admin
-router.get('/getAllCombos', async (req, res) => {
+router.get("/getAllCombos", async (req, res) => {
   try {
     const data = await Combos.find().sort({ Ordinal: 1 });
     res.status(200).json(data);
@@ -24,16 +24,16 @@ router.get('/getAllCombos', async (req, res) => {
 });
 
 //get One
-router.get('/getOneCombos', async (req,res) => {
-  try{
+router.get("/getOneCombos", async (req, res) => {
+  try {
     const data = Combos.findById(req.body._id);
-    res.status(200).json(data)
-  }catch(err) {
+    res.status(200).json(data);
+  } catch (err) {
     res.status(400).json(err);
   }
-})
+});
 // get combos for user
-router.get('/getCombos', async (req, res) => {
+router.get("/getCombos", async (req, res) => {
   try {
     const data = await Combos.aggregate([
       {
@@ -41,26 +41,58 @@ router.get('/getCombos', async (req, res) => {
           Is_Show: true,
         },
       },
-     {
-      $lookup: {
-        from: 'products',
-        localField: 'Arr_Id_Products',
-        foreignField: '_id',
-        as: 'Arr_Products',
+      {
+        $lookup: {
+          from: "products",
+          localField: "Arr_Id_Products",
+          foreignField: "_id",
+          as: "Arr_Products",
+        },
       },
-    },
-    {
-      $project: {
-        Arr_Id_Products: 0,
+      {
+        $project: {
+          Arr_Id_Products: 0,
+        },
       },
-    },
-  ]).sort({ Ordinal: 1 });
+    ]).sort({ Ordinal: 1 });
     res.status(200).json(data);
   } catch (err) {
     res.status(400).json(err);
   }
 });
+// get 1 combo for user
+router.get("/getOneCombo/:id", async (req, res) => {
+  try {
+    const data = await Combos.aggregate([
+      {
+        $match: {
+          _id: new mongoose.Types.ObjectId(req.params.id),
+        },
+      },
+      {
+        $lookup: {
+          from: "products",
+          localField: "Arr_Id_Products",
+          foreignField: "_id",
+          as: "Arr_Products",
+        },
+      },
+      {
+        $project: {
+          Arr_Id_Products: 0,
+        },
+      },
+    ]);
+    // set views +1
+    await Combos.findByIdAndUpdate(req.params.id, {
+      $inc: { Views: 1 },
+    });
 
+    res.status(200).json(data[0]);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
 //update
 router.put("/", async (req, res) => {
@@ -77,15 +109,13 @@ router.put("/", async (req, res) => {
 });
 
 //delete
-router.delete('/',async (req,res) => {
+router.delete("/", async (req, res) => {
   try {
-    await Combos.findByIdAndDelete(
-      { _id: req.body._id }
-    );
-    res.status(200).json('delete success!!');
+    await Combos.findByIdAndDelete({ _id: req.body._id });
+    res.status(200).json("delete success!!");
   } catch (err) {
     res.status(400).json(err);
   }
-})
+});
 
 module.exports = router;
